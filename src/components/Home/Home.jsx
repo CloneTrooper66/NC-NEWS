bb4b15cecd7d41d02b0d55904eac4e79358d80a1
CloneTrooper66 <luka.eli88@gmail.com>
2024-08-03 22:23:18 +0100

    adds nav section with responsive design

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function () {
+  return (
+    <div className="container">
+      <h1 className="heading">Welcome to My Website!</h1>
+      <p className="description">I'm excited to have you here.</p>
+      {/* <Link to="/login">Get Started</Link> */}
+    </div>
+  );
+}
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "../../styles/Nav.css";
+
+export default function Nav() {
+  function showSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+  }
+
+  function hideSidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+  }
+
+  return (
+    <>
+      <nav>
+        <ul className="sidebar">
+          <li onClick={hideSidebar}>
+            <a href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="26px"
+                viewBox="0 -960 960 960"
+                width="26px"
+                fill="#5f6368"
+              >
+                <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+          <li>
+            <a href="#">Topics</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+
+        <ul>
+          <li>
+            <a href="#">NC NEWS</a>
+          </li>
+          <li className="hideOnMobile">
+            <a href="#">Home</a>
+          </li>
+          <li className="hideOnMobile">
+            <a href="#">Articles</a>
+          </li>
+          <li className="hideOnMobile">
+            <a href="#">Topics</a>
+          </li>
+          <li className="hideOnMobile">
+            <a href="#">Login</a>
+          </li>
+          <li className="menu-button" onClick={showSidebar}>
+            <a href="#">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="26px"
+                viewBox="0 -960 960 960"
+                width="26px"
+                fill="#5f6368"
+              >
+                <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
